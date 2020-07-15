7c18616c54c8359bb63573a41f8868b0e9a8810b
florianmathieudev <florianmathieu.dev@gmail.com>
2020-07-15 12:47:22 +0200

    form ok

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
  */
 var app = {
   init: function() {
+    app.todo = document.getElementById('todo');
     //Création du form
     app.createForm();
     //Creéation du compteur
@@ -13,14 +14,32 @@ var app = {
   //Création du form
   createForm: function() {
     console.log('app createForm');
+    //balise <form> +enrichissement(id...)
+    var form = document.createElement('form')
+    form.id = 'todo-form';
+    //balise <input> text + placeholder et id
+    var input = document.createElement('input');
+    input.id = 'todo-input';
+    input.placeholder = 'Ajouter une tâche';
+    input.type = 'text';
+    //placer input dans le form
+    form.appendChild(input);
+    //placer le form dans le DOM
+    app.todo.appendChild(form);
   },
-  //Creéation du compteur
+  //Création du compteur
   createCounter: function() {
-    console.log(app.createCounter);
+    console.log('app createCounter');
+
+    //balise <p>
+    var p = document.createElement
+    //balise <span>
+
+    //mettre span dans p
   },
   //Création de la liste
   createList: function() {
-    console.log(app.createList);
+    console.log('app createList');
   },
 };
 
